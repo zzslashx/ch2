59a185d976e42a272594c839d672c5076f1028ad
𝓥𝓮𝓲𝓵 <2054575@learn.vsb.bc.ca>
2026-09-14 14:50:43 -0700

    Add files via upload
    Exercise 2.2

---
+++ date.java
@@ -0,0 +1,10 @@
+public class date {
+public static void main (String[] args){
+	String day = "Monday";
+	int date = 14;
+	String month = "September";
+	int year = 2026;
+	System.out.println("American Format: " + day + ", " + month + " " +date + ", " + year);
+	System.out.println("European Format: " + day + ", " + date + " "+ month + ", " + year);
+	}
+}
