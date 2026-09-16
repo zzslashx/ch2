e1a6a6d2a1b3430c2b08b8764f06a0859f6bd2d2
𝓥𝓮𝓲𝓵 <2054575@learn.vsb.bc.ca>
2026-09-15 21:59:16 -0700

    Example 1

---
+++ Identity.java
@@ -0,0 +1,9 @@
+public class Identity{
+	public static void main(String[] args){
+		
+		int age = 15;
+		String name = "Cheng Cheng";
+		System.out.println("My name is " + name + " and I am " +  "\"" + age+ "\"" + " years old."); 
+		
+	}
+}
